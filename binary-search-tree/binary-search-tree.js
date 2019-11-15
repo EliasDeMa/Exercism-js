57507dfc0b1edf347b815db6562bf9c11d8f519a
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2019-11-15 11:42:26 +0100

    binary search tree exercise

---
+++ binary-search-tree/binary-search-tree.js
@@ -0,0 +1,73 @@
+//
+// This is only a SKELETON file for the 'Binary Search Tree' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export class BinarySearchTree {
+  constructor(data) {
+    this.root = new Node(data);
+  }
+
+  get data() {
+    return this.root.data;
+  }
+
+  get right() {
+    return this.root.right;
+  }
+
+  get left() {
+    return this.root.left;
+  }
+
+  insert(data) {
+    let node = this.root;
+    let finished = false;
+    while (!finished) {
+      if (data > node.data) {
+        if (node.right !== null) {
+          node = node.right;
+        }
+        else {
+          node.right = new Node(data);
+          finished = true;
+        }
+      } else {
+        if (node.left !== null) {
+          node = node.left;
+        }
+        else {
+          node.left = new Node(data);
+          finished = true;
+        }
+      }
+    }    
+  }
+
+  each(callback) {
+    return BinarySearchTree.traverse(this.root).map(callback);
+  }
+
+  static traverse(node) {
+    let result = [];
+    
+    if (node.left) {
+      result.push(...BinarySearchTree.traverse(node.left))
+    }
+
+    result.push(node.data);
+
+    if (node.right) {
+      result.push(...BinarySearchTree.traverse(node.right))
+    }
+    
+    return result;
+  }
+}
